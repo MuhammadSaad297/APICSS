8ad35c72699af5d76055d78603d1e3c3fd011ec4
computer <computer@DESKTOP-MFVI5FD>
2022-11-01 18:30:58 +0500

    Add project files.

---
+++ AssignmentApiCSS/Controllers/CSSController.cs
@@ -0,0 +1,22 @@
+﻿using AssignmentApiCSS.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AssignmentApiCSS.Controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class CSSController : ControllerBase
+    {
+        public CSSServices _Services { get; set; }
+        public CSSController(CSSServices cSSServices)
+        {
+            _Services = cSSServices;
+        }
+        [HttpGet]
+        public string Get()
+        {
+            return _Services.getCss();
+        }
+    }
+}
